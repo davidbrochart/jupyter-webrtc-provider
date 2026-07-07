0359745f7959a3503a5468f0b1ee79f0fe5c0d6d
David Brochart <david.brochart@gmail.com>
2026-07-07 16:53:30 +0200

    lint

--- src/webrtc.ts
+++ src/webrtc.ts
@@ -655,7 +655,7 @@ export class SignalingConn extends WebsocketClient {
         case 'publish': {
           const roomName = m.topic;
           const room = rooms.get(roomName);
-          if (room && (m.data.from === room.peerId) && (m.clients === 1)) {
+          if (room && m.data.from === room.peerId && m.clients === 1) {
             const parsed = await this.roomIdManager.parseRoomId(roomName);
             if (parsed) {
               const { format, contentType, path } = parsed;
